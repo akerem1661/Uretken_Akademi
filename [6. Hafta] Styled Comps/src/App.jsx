1101b2b37fc397292d3e02020c9d6a379935d1d3
akerem1661 <yadaolmusumdehaberimyok@gmail.com>
2024-02-24 19:32:38 +0300

    [6. Hafta] Styled Comps

---
+++ [6. Hafta] Styled Comps/src/App.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import GlobalStyle from "./globalStyles";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import Home from "./pages/Home";
+import SignUp from "./pages/SignupPage";
+import Pricing from "./pages/PricingPage";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <GlobalStyle />
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/pricing" element={<Pricing />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </>
+  );
+}
+
+export default App;
